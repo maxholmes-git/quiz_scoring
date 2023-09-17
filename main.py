62810fad38061337b94647da0537eddbf24bbf53
MaxHolmes <maxholmes518@gmail.com>
2023-09-17 15:53:24 +0100

    Reorganized functions relating to final scoring and aggregate difficulty round scoring

--- main.py
+++ main.py
@@ -21,60 +21,62 @@ def score_logic(player, player_list):
             The polars logic.
     """
     logic = pl.when(pl.col("Round_type") == "least_popular").then(least_popular_logic(player, player_list))\
-              .when(pl.col("Round_type") == "aggregate_difficulty").then(aggregate_difficulty_logic(player))
+              .when(pl.col("Round_type") == "aggregate_difficulty").then(aggregate_difficulty_logic(player))#\
+              #.when(pl.col("Round_type") == "text").then()
     return logic
 
 
-def score_aggregate_difficulty_rounds(df):
-    aggregate_difficulty_df = df.filter(pl.col("Round_type") == "aggregate_difficulty")
-    if not aggregate_difficulty_df.is_empty():
-        aggregate_difficulty_rounds = set([row["Round"] for row in aggregate_difficulty_df.iter_rows(named=True)])
-        #Need to come up with something better than this to empty the dataframe
-        final_df = aggregate_difficulty_df.null_count().filter(pl.col("Round") == pl.lit(1000))\
-            .with_columns(pl.col("Round").cast(str))
-        if len(aggregate_difficulty_rounds) > 1:
-            for round_number in aggregate_difficulty_rounds:
-                round_df = aggregate_difficulty_df.filter(pl.col("Round") == pl.lit(round_number))
-                null_count_df = round_df.null_count()
-                null_count_df = null_count_df.with_columns(pl.lit(round_number).alias("Round"))
-                final_df = pl.concat([final_df, null_count_df], rechunk=True)
-
-        else:
-            null_count_df = aggregate_difficulty_df.null_count()
-            final_df = null_count_df.with_columns(
-                pl.lit(next(iter(aggregate_difficulty_rounds))).alias("Round")
-            )
-        return final_df
-    return aggregate_difficulty_df
+def count_nulls_per_round(aggregate_difficulty_df):
+    aggregate_difficulty_rounds = set([row["Round"] for row in aggregate_difficulty_df.iter_rows(named=True)])
+    #Need to come up with something better than this to empty the dataframe
+    all_null_counts_df = aggregate_difficulty_df.null_count().filter(pl.col("Round") == pl.lit(1000))\
+        .with_columns(pl.col("Round").cast(str))
+    for round_number in aggregate_difficulty_rounds:
+        round_df = aggregate_difficulty_df.filter(pl.col("Round") == pl.lit(round_number))
+        null_count_df = round_df.null_count()
+        null_count_df = null_count_df.with_columns(pl.lit(round_number).alias("Round"))
+        all_null_counts_df = pl.concat([all_null_counts_df, null_count_df], rechunk=True)
+    return all_null_counts_df
 
 
-def create_aggregate_scoring_df(df, player_list):
-    null_count_df = score_aggregate_difficulty_rounds(df)
+def score_aggregate_difficulty_rounds(aggregate_difficulty_df, round_scoring_df, player_list):
+    all_null_counts_df = count_nulls_per_round(aggregate_difficulty_df)
 
-    aggregate_df = df.select([f"{player}_score" for player in player_list] + ["Round"])\
-        .group_by("Round", maintain_order=True).sum()
-    if not null_count_df.is_empty():
-        aggregate_df = aggregate_df\
-            .join(null_count_df.select(["Round"] + [f"{player}_score" for player in player_list]),
-                  on=["Round"], how="left", suffix="_null_count")\
-            .with_columns(
+    round_scoring_df = round_scoring_df\
+        .join(all_null_counts_df.select(["Round"] + [f"{player}_score" for player in player_list]),
+              on=["Round"], how="left", suffix="_null_count") \
+        .with_columns(
             [pl.when(pl.col(f"{player}_score_null_count") == 0)
              .then(None)
              .otherwise(pl.col(f"{player}_score_null_count"))
              .alias(f"{player}_score_null_count")
              for player in player_list]
         )
-        aggregate_df = aggregate_df.with_columns(
+    round_scoring_df = round_scoring_df\
+        .with_columns(
             [pl.when(pl.col(f"{player}_score_null_count").is_null())
-             .then(pl.col(f"{player}_score"))\
+             .then(pl.col(f"{player}_score"))
              .otherwise((pl.col(f"{player}_score") / pl.col(f"{player}_score_null_count")).round(0).cast(int))
              .alias(f"{player}_score")
              for player in player_list]
-        )
+    )
 
-    grand_total_df = aggregate_df.sum()\
+    return round_scoring_df
+
+
+def create_round_scoring_df(df, player_list):
+    round_scoring_df = df.select([f"{player}_score" for player in player_list] + ["Round"])\
+        .group_by("Round", maintain_order=True).sum()
+
+    aggregate_difficulty_df = df.filter(pl.col("Round_type") == "aggregate_difficulty")
+    if not aggregate_difficulty_df.is_empty():
+        round_scoring_df = score_aggregate_difficulty_rounds(aggregate_difficulty_df,
+                                                             round_scoring_df,
+                                                             player_list)
+
+    grand_total_df = round_scoring_df.sum()\
         .with_columns(pl.when(pl.col("Round").is_null()).then(pl.lit("Grand total")).alias("Round"))
-    final_df = pl.concat([aggregate_df, grand_total_df], rechunk=True)
+    final_df = pl.concat([round_scoring_df, grand_total_df], rechunk=True)
     return final_df
 
 
@@ -98,6 +100,6 @@ if __name__ == "__main__":
                   [f"{name}_score" for name in player_list])
           )
 
-    aggregate_scoring_df = create_aggregate_scoring_df(master_df, player_list)
+    aggregate_scoring_df = create_round_scoring_df(master_df, player_list)
     print(aggregate_scoring_df)
     #delete_files_in_directory(player_list, score_sheets_path)
